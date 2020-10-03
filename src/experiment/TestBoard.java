f9c44795509cc4f4fc9d45759a9bf5e74cb78682
Nathan Millwater <nmillwater@mymail.mines.edu>
2020-10-02 18:56:52 -0600

    Added stub methods in experiment package
    Added methods necessary for JUnit testing class

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -1,5 +1,19 @@
 package experiment;
 
-public class TestBoard {
+import java.util.*;
 
+public class TestBoard {
+	
+	
+	public void calcTargets(TestBoardCell startCell, int pathlength) {
+		
+	}
+	
+	public Set<TestBoardCell> getTargets() {
+		return new TreeSet<TestBoardCell>();
+	}
+	
+	public TestBoardCell getCell(int row, int col) {
+		return new TestBoardCell(0,0);
+	}
 }
--- src/experiment/TestBoardCell.java
+++ src/experiment/TestBoardCell.java
@@ -1,5 +1,34 @@
 package experiment;
 
-public class TestBoardCell {
+import java.util.*;
 
+public class TestBoardCell {
+	
+	
+	public TestBoardCell(int row, int column) {
+		super();
+	}
+	
+	// Setters and Getters
+	public Set<TestBoardCell> getAdjList() {
+		return new TreeSet<TestBoardCell>();
+	}
+	
+	public void setRoom(boolean room) {
+		
+	}
+	
+	public boolean isRoom() {
+		return false;
+	}
+
+	public boolean isOccupied() {
+		return false;
+	}
+
+	public void setOccupied(boolean occupied) {
+		
+	}
+
+	
 }
