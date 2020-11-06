8f79e601ef5e1812749fea250c85b38d4238023e
Nathan Millwater <nmillwater@mymail.mines.edu>
2020-11-05 19:21:03 -0700

    Updated GameControlPanel with GUI components for Clue game

--- src/clueGame/GameControlPanel.java
+++ src/clueGame/GameControlPanel.java
@@ -8,20 +8,28 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
 
 public class GameControlPanel extends JFrame {
 	
+	private JTextField result;
+	private JTextField guess;
+	private JTextField roll;
+	private JTextField turnDescription;
 	
 	public GameControlPanel() {
 		JPanel gameControlPanel = new JPanel();
 		JPanel firstRow = new JPanel();
+		firstRow.setLayout(new GridLayout());
 		JPanel secondRow = new JPanel();
+		secondRow.setLayout(new GridLayout(1,2));
 		
-		gameControlPanel.setLayout(new GridLayout(2,0));
+		gameControlPanel.setLayout(new GridLayout(2,1));
 		gameControlPanel.add(firstRow);
 		gameControlPanel.add(secondRow);
 		
-		firstRow.setLayout(new GridLayout());
+		// add items to first row
 		JPanel whoseTurn = createWhoseTurn();
 		firstRow.add(whoseTurn);
 		JPanel roll = createRoll();
@@ -31,27 +39,70 @@ public class GameControlPanel extends JFrame {
 		JButton next = new JButton("Next!");
 		firstRow.add(next);
 		
+		// add items to second row
+		JPanel resultPanel = createGuessPanel();
+		JPanel guessPanel = createResultPanel();
+		secondRow.add(guessPanel);
+		secondRow.add(resultPanel);
+		
 		add(gameControlPanel, BorderLayout.CENTER);
+	}
+
+	private JPanel createResultPanel() {
+		JPanel resultPanel = new JPanel();
+		resultPanel.setLayout(new GridLayout());
+		resultPanel.setBorder(new TitledBorder(new EtchedBorder(), "Guess Result"));
+		result = new JTextField();
+		result.setEditable(false);
+		resultPanel.add(result);
+		return resultPanel;
+	}
+
+	private JPanel createGuessPanel() {
+		JPanel guessPanel = new JPanel();
+		guessPanel.setLayout(new GridLayout());
+		guessPanel.setBorder(new TitledBorder(new EtchedBorder(), "Guess"));
+		guess = new JTextField();
+		guess.setEditable(false);
+		guessPanel.add(guess);
+		return guessPanel;
 	}
 	
 	private JPanel createRoll() {
-		JPanel roll = new JPanel();
-		roll.setLayout(new GridLayout(1,2));
+		JPanel rollPanel = new JPanel();
 		JLabel label = new JLabel("Roll:");
-		JTextField num = new JTextField("5");
-		roll.add(label);
-		roll.add(num);
-		return roll;
+		roll = new JTextField(5);
+		roll.setEditable(false);
+		rollPanel.add(label);
+		rollPanel.add(roll);
+		return rollPanel;
 	}
 
 	private JPanel createWhoseTurn() {
-		JPanel whoseTurn = new JPanel();
-		whoseTurn.setLayout(new GridLayout(2,1));
+		JPanel whoseTurnPanel = new JPanel();
 		JLabel label = new JLabel("Whose Turn?");
-		JTextField player = new JTextField("sdfsd");
-		whoseTurn.add(label);
-		whoseTurn.add(player);
-		return whoseTurn;
+		turnDescription = new JTextField(16);
+		turnDescription.setEditable(false);
+		whoseTurnPanel.add(label);
+		whoseTurnPanel.add(turnDescription);
+		return whoseTurnPanel;
+	}
+
+	// setters 
+	public void setResult(String r) {
+		result.setText(r);
+	}
+
+	public void setGuess(String g) {
+		guess.setText(g);
+	}
+
+	public void setRoll(String r) {
+		roll.setText(r);
+	}
+
+	public void setTurnDescription(String d) {
+		turnDescription.setText(d);
 	}
 
 	public static void main(String[] args) {
@@ -60,6 +111,11 @@ public class GameControlPanel extends JFrame {
 		control.setTitle("Control Panel For Clue");
 		control.setVisible(true);
 		control.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		control.setGuess("Blue");
+		control.setTurnDescription("I don't have a guess. Sorry :(");
+		control.setRoll("3");
+		control.setResult("You lose then");
 	}
 
 }
